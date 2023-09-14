558c86fd5280ead47f86b7b836edbf10c715907d
omrip500 <omrip500@gmail.com>
2023-09-14 11:12:11 +0300

    commit

--- server.js
+++ server.js
@@ -16,29 +16,29 @@ app.use(bodyParser.json());
 app.use(cors());
 
 ///////////////////////Socket.io initialzing////////////////////////////
-const http = require("http");
-const { Server } = require("socket.io");
+// const http = require("http");
+// const { Server } = require("socket.io");
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  socket.on("sendMessage", (data) => {
-    console.log("Message: " + data.message);
-    console.log("LectureNumber: " + data.lectureNumber);
-    socket.broadcast.emit("receiveMessage", data);
-  });
-});
+// io.on("connection", (socket) => {
+//   socket.on("sendMessage", (data) => {
+//     console.log("Message: " + data.message);
+//     console.log("LectureNumber: " + data.lectureNumber);
+//     socket.broadcast.emit("receiveMessage", data);
+//   });
+// });
 
-server.listen(8081, () => {
-  console.log("Socket server is running");
-});
+// server.listen(8081, () => {
+//   console.log("Socket server is running");
+// });
 
 ////////////////////////////////////////////////////////////////////////
 let multerFileName;
